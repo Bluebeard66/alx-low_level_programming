efe8e99bbdd4157de3fc9888557783e79439056f
Blue <127500156+Bluebeard66@users.noreply.github.com>
2023-07-15 02:52:23 +0300

    task-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if !(a == '0')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
