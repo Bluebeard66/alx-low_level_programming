e5d62a141596d38adecd24b9ec56d1ba2ad92899
Blue <127500156+Bluebeard66@users.noreply.github.com>
2023-07-15 03:19:44 +0300

    adv-task_101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+/**
+ * main - print all possible different combinations of three digits.
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int hands = 0, tens, ones;
+
+	for (hands = 0; hands <= 9; hands++)
+	{
+		for (tens = 0; tens <= 9; tens++)
+		{
+			for (ones = 0; ones <= 9; ones++)
+			{
+				if (hands != tens && hends != ones && tens != ones
+&& hands < tens && tens < ones)
+				{
+					putchar(hands + '0');
+					putchar(tens + '0');
+					putchar(ones + '0');
+					if (!((tens * 100) + (tens * 10) + ones == 89))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
