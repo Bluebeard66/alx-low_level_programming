85496bd7806296f345a016fb6bdfb4004e02be45
Blue <127500156+Bluebeard66@users.noreply.github.com>
2023-07-15 03:11:36 +0300

    adv-task_100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main - print all possible different combinations of two digits.
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int tens = 0, ones;
+
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ones = 0; ones <= 9; ones++)
+		{
+			if (tens != ones && tens < ones)
+			{
+				putchar(tens + '0');
+				putchar(ones + '0');
+				if (tens + ones == 89)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
