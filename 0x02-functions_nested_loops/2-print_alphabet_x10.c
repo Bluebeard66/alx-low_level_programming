0f1c257389e031bc0ad102f337366bdbf3678724
Blue <127500156+Bluebeard66@users.noreply.github.com>
2023-07-19 05:35:33 +0300

    task-3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabet 10 times in lowercase.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		print_alphaber();
+}
