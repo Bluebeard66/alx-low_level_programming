e59680321e142110a864417f791dcbb1d5e5f916
Blue <127500156+Bluebeard66@users.noreply.github.com>
2023-07-19 06:17:06 +0300

    task-4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - checks if the character is alphabet
+ * Return: 1 if c is alpha
+ * else 0
+ * @c: takes character as input and cast it as int
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'Z')
+		return (1);
+	return (0);
+}
