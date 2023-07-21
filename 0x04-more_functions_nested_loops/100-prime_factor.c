0f63740f50fe94649c40af014f33f143600d64f8
Blue <127500156+Bluebeard66@users.noreply.github.com>
2023-07-21 20:50:32 +0300

    task-100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,27 +1,18 @@
 #include <stdio.h>
 /**
-* main- the largest prime factor of the number 612852475143
-* Return: 0
-*/
+ * main - prints the largest prime factor of 612852475143
+ * Return: 0
+ **/
 int main(void)
 {
 	long n = 612852475143;
-	int c;
+	int i;
 
-	while (c++ < n / 2)
+	for (i = 3; i < 782849; i = i + 2)
 	{
-		if (n % c == 0)
-		{
-			n = n / 2;
-			continue;
-		}
-		for (c = 3; c < n / 2; c += 2)
-		{
-			if (n % c == 0)
-			n = n / c;
-
-		}
+		while ((n % i == 0) && (n != i))
+			n = n / i;
 	}
-	printf("%ld\n", n);
+	printf("%lu\n", n);
 	return (0);
 }
