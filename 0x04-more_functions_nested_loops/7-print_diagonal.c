2608361794bce72c9f7bb7b5faf77734e35edde4
Blue <127500156+Bluebeard66@users.noreply.github.com>
2023-07-21 20:35:46 +0300

    task-7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,16 +6,15 @@
 void print_diagonal(int n)
 {
 	int i, c;
+
 	if (n > 0)
 	for (c = 0; c < n; c++)
-		{
+	{
 		for (i = 0; i < c; i++)
-			{
-				_putchar(' ');
-			}
+			_putchar(' ');
 		_putchar('\\');
 		_putchar('\n');
-		}
+	}
 	else
 		_putchar('\n');
 }
