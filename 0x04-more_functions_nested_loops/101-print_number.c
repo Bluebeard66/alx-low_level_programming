a861e8bd84249bdabe7febb4b8e1f495c4dbbbfc
Blue <127500156+Bluebeard66@users.noreply.github.com>
2023-07-21 06:13:46 +0300

    task-101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_number - prints an integer.
+* @n: takes integer as input to print
+*/
+void print_number(int num)
+{
+	unsigned int i = num;
+	if (n < 0)
+	{
+	_putchar('-');
+	i = -i;
+	}
+	if (i > 9)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
