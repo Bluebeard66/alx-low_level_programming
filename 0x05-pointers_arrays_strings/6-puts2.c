0f5ece0ed981920e14a4473f467309738c75533c
Blue <127500156+Bluebeard66@users.noreply.github.com>
2023-07-25 06:46:09 +0300

    task-6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts2 - one char out of 2 of a string
+ * @st: string to print the chars from
+ */
+void puts2(char *str)
+{
+	int len, i;
+
+	len = 0;
+
+	while (str[len] != '\0')
+
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
