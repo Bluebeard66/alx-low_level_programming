1965ef938da5566d6fc4b61c17a80c69f5e2ddb3
Blue <127500156+Bluebeard66@users.noreply.github.com>
2023-07-29 06:28:51 +0300

    adv

---
+++ 100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - the encoder rot13
+ * @s: pointer to string
+ *
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char origtext[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char ciphertext[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == origtext[j])
+			{
+				s[i] = ciphertext[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
