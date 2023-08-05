6c5ed00edd9a40477f3ec2102ca15fead1d9fac1
Blue <127500156+Bluebeard66@users.noreply.github.com>
2023-08-05 06:18:33 +0300

    new project

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ */
+void main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0];
+}
